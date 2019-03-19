8f5e0c88b7c199e8956b21f132c2a3898b8ada09
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-03-19 09:47:22 +0800

    first commit

---
+++ app/src/main/java/com/imooc/nick/cardtestproject/bean/EmotionBean.java
@@ -0,0 +1,14 @@
+package com.imooc.nick.cardtestproject.bean;
+
+import android.graphics.Bitmap;
+
+public class EmotionBean {
+    public Bitmap bitmap;
+
+    public int x,y;
+
+    public int velocityX,velocityY;
+
+    public int appearTimestamp;
+
+}
